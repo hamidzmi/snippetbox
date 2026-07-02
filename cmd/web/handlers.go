c147a0c5e3eeb94e1b19a9de27ea8dc1651e0155
Hamid Zamani <hamidzamani.ce@gmail.com>
2026-07-02 14:27:00 +0200

    new project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +11,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -22,7 +20,6 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
@@ -32,17 +29,5 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
-	w.Write(([]byte("Create a new snippet...")))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	w.Write([]byte("Create a new snippet..."))
 }
